9934fb15af3102d4959e1be82ec610cb84718174
PavlenkoEvgeniy <pavlenkoevgeniy85@gmail.com>
2023-10-04 09:07:34 +0300

    add Breadcrumbs

--- app/controllers/AppController.php
+++ app/controllers/AppController.php
@@ -6,9 +6,11 @@ use app\models\AppModel;
 use app\widgets\language\Language;
 use core\App;
 use core\Controller;
+use RedBeanPHP\R;
 
 class AppController extends Controller
 {
+
     public function __construct($route)
     {
         parent::__construct($route);
@@ -19,5 +21,12 @@ class AppController extends Controller
 
         $lang = App::$app->getProperty('language');
         \core\Language::load($lang['code'], $this->route);
+
+        $categories = R::getAssoc("SELECT c.*, cd.* FROM category c 
+                        JOIN category_description cd
+                        ON c.id = cd.category_id
+                        WHERE cd.language_id = ?", [$lang['id']]);
+        App::$app->setProperty("categories_{$lang['code']}", $categories);
     }
+
 }
--- app/widgets/menu/Menu.php
+++ app/widgets/menu/Menu.php
@@ -8,6 +8,7 @@ use core\Cache;
 
 class Menu
 {
+
     protected $data;
     protected $tree;
     protected $menuHtml;
@@ -20,16 +21,14 @@ class Menu
     protected $prepend = '';
     protected $language;
 
-    public function __construct($options = [])
-    {
+    public function __construct($options = []){
         $this->language = App::$app->getProperty('language');
         $this->tpl = __DIR__ . '/menu_tpl.php';
         $this->getOptions($options);
         $this->run();
     }
 
-    protected function getOptions($options)
-    {
+    protected function getOptions($options){
         foreach($options as $k => $v){
             if(property_exists($this, $k)){
                 $this->$k = $v;
@@ -37,16 +36,12 @@ class Menu
         }
     }
 
-    protected function run()
-    {
+    protected function run(){
         $cache = Cache::getInstance();
         $this->menuHtml = $cache->get("{$this->cacheKey}_{$this->language['code']}");
 
         if(!$this->menuHtml){
-            $this->data = R::getAssoc("SELECT c.*, cd.* FROM category c 
-                        JOIN category_description cd
-                        ON c.id = cd.category_id
-                        WHERE cd.language_id = ?", [$this->language['id']]);
+            $this->data = App::$app->getProperty("categories_{$this->language['code']}");
             $this->tree = $this->getTree();
             $this->menuHtml = $this->getMenuHtml($this->tree);
             if($this->cache){
@@ -57,8 +52,7 @@ class Menu
         $this->output();
     }
 
-    protected function output()
-    {
+    protected function output(){
         $attrs = '';
         if(!empty($this->attrs)){
             foreach($this->attrs as $k => $v){
@@ -71,8 +65,7 @@ class Menu
         echo "</{$this->container}>";
     }
 
-    protected function getTree()
-    {
+    protected function getTree(){
         $tree = [];
         $data = $this->data;
         foreach ($data as $id=>&$node) {
@@ -85,8 +78,7 @@ class Menu
         return $tree;
     }
 
-    protected function getMenuHtml($tree, $tab = '')
-    {
+    protected function getMenuHtml($tree, $tab = ''){
         $str = '';
         foreach($tree as $id => $category){
             $str .= $this->catToTemplate($category, $tab, $id);
@@ -94,10 +86,10 @@ class Menu
         return $str;
     }
 
-    protected function catToTemplate($category, $tab, $id)
-    {
+    protected function catToTemplate($category, $tab, $id){
         ob_start();
         require $this->tpl;
         return ob_get_clean();
     }
+
 }
