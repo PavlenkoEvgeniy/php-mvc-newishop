872e6634a041d14d4e9256986e401121f471b556
PavlenkoEvgeniy <pavlenkoevgeniy85@gmail.com>
2023-10-18 10:53:25 +0300

    add: search for products

--- config/routes.php
+++ config/routes.php
@@ -2,15 +2,17 @@
 
 use core\Router;
 
-//admin
+//admin route:
 Router::add('^admin/?$', ['controller' => 'Main', 'action' => 'index', 'admin_prefix' => 'admin']);
 
 Router::add('^admin/(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$', ['admin_prefix' => 'admin']);
 
-//other
+//another routes:
 Router::add('^(?P<lang>[a-z]+)?/?product/(?P<slug>[a-z0-9-]+)/?$', ['controller' => 'Product', 'action' => 'view']);
 
 Router::add('^(?P<lang>[a-z]+)?/?category/(?P<slug>[a-z0-9-]+)/?$', ['controller' => 'Category', 'action' => 'view']);
+
+Router::add('^(?P<lang>[a-z]+)?/?search/?$', ['controller' => 'Search', 'action' => 'index']);
 
 Router::add('^(?P<lang>[a-z]+)?/?$', ['controller' => 'Main', 'action' => 'index']);
 
