11466c861c4d11c05902ee4c1a08c96343271378
PavlenkoEvgeniy <pavlenkoevgeniy85@gmail.com>
2023-10-26 22:54:56 +0300

    update: apdim page - finished to integrate adminlte3

--- app/controllers/admin/AppController.php
+++ app/controllers/admin/AppController.php
@@ -2,9 +2,22 @@
 
 namespace app\controllers\admin;
 
+use app\models\AppModel;
+use app\widgets\language\Language;
+use core\App;
 use core\Controller;
 
 class AppController extends Controller
 {
+
     public false|string $layout = 'admin';
+
+    public function __construct($route)
+    {
+        parent::__construct($route);
+        new AppModel();
+        App::$app->setProperty('languages', Language::getLanguages());
+        App::$app->setProperty('language', Language::getLanguage(App::$app->getProperty('languages')));
+    }
+
 }
--- app/views/admin/parts/footer.php
+++ app/views/admin/parts/footer.php
@@ -6,9 +6,9 @@
     <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
 </footer>
 
-<!--<div class="logs">-->
-<!--    --><?php //$this->getDbLogs(); ?>
-<!--</div>-->
+<div class="logs">
+    <?php $this->getDbLogs(); ?>
+</div>
 
 <!-- Control Sidebar -->
 <aside class="control-sidebar control-sidebar-dark">
