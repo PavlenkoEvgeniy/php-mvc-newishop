e90bb01d5c81960b050715e9776af5a7dc3c5b52
PavlenkoEvgeniy <pavlenkoevgeniy85@gmail.com>
2023-10-30 07:07:06 +0300

    add: admin - product list

---
+++ app/controllers/admin/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\controllers\admin;
+
+use app\models\admin\Product;
+use core\App;
+use core\Pagination;
+use RedBeanPHP\R;
+
+/** @property Product $model*/
+class ProductController extends AppController
+{
+
+    public function indexAction()
+    {
+        $lang = App::$app->getProperty('language');
+        $page = get('page');
+        $perpage = 3;
+        $total = R::count('product');
+        $pagination = new Pagination($page, $perpage, $total);
+        $start = $pagination->getStart();
+
+        $products = $this->model->get_products($lang, $start, $perpage);
+        $title = 'Список товаров';
+        $this->setMeta("Админка :: {$title}");
+        $this->set(compact('title', 'products', 'pagination', 'total'));
+    }
+
+}
