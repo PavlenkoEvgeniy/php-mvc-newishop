623acd2b47f9134cf9988a0e3f73c28a21178ce2
PavlenkoEvgeniy <pavlenkoevgeniy85@gmail.com>
2023-11-03 02:47:09 +0300

    add: admin - digital products list

---
+++ app/controllers/admin/DownloadController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace app\controllers\admin;
+
+use app\models\admin\Download;
+use core\App;
+use core\Pagination;
+use RedBeanPHP\R;
+
+/** @property Download $model */
+class DownloadController extends AppController
+{
+
+    public function indexAction()
+    {
+        $lang = App::$app->getProperty('language');
+        $page = get('page');
+        $perpage = 20;
+        $total = R::count('download');
+        $pagination = new Pagination($page, $perpage, $total);
+        $start = $pagination->getStart();
+
+        $downloads = $this->model->get_downloads($lang, $start, $perpage);
+        $title = 'Файлы (цифровые товары)';
+        $this->setMeta("Админка :: {$title}");
+        $this->set(compact('title', 'downloads', 'pagination', 'total'));
+    }
+
+}
